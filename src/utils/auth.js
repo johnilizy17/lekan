59d966020a10ef2ebbdac9d21498e6d868523081
johnilizy17 <johnabrahamtosin@gmail.com>
2023-10-03 09:03:11 +0100

    url correction

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,17 +2,4 @@ import axios from "axios";
 
 export const authService = axios.create({ baseURL: "https://nysc.onrender.com" });
 
-authService.interceptors.request.use(
-	(config) => {
-		let token
-		token = localStorage.getItem("token");
-		console.log("token here", token)
-
-		if (token == null) {
-			config.headers.common["Authorization"] = `Bearer ${token}`;
-			return config;
-		}
-		return config;
-	},
-);
 
